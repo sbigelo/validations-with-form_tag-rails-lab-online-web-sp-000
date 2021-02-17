4cfc59abd67a19be397e881e14b602b49a40ddcf
sbigelo <sean.bigelow1@gmail.com>
2021-02-17 17:23:46 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 100}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
